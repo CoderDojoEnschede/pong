48dbc79575d0b4c18eacdee17496238af7be25e3
Hidde Wieringa <hidde@hiddewieringa.nl>
2021-07-11 13:48:34 +0200

    Opdrachten kommentaar en kleine verbeteringen

--- pong.js
+++ pong.js
@@ -6,13 +6,26 @@ const context = canvas.getContext('2d');
 // Voor de dikte van de muur, de grootte van het balletje en de hoogte van de batjes en de extra ruimte her en der.
 const roosterGrootte = 15;
 
-const speelveld = {x: 0, y: 0, breedte: canvas.width, hoogte: canvas.height};
+// We stellen de maat van het canvas in aan de hand van de roostergrootte
+canvas.width = 57 * roosterGrootte;
+canvas.height = 35 * roosterGrootte;
+const speelveld = {
+    x: 0,
+    y: 0,
+    breedte: canvas.width,
+    hoogte: canvas.height,
+};
 
 const hoogteVanBatje = roosterGrootte * 5;
 const maximaleVerticalePositieVanBatje = speelveld.hoogte - roosterGrootte - hoogteVanBatje;
 
 const snelheidVanBatje = 12;
+
+// --- Opdracht 7 --- //
+
 const snelheidVanBalletje = 3;
+
+// --- Opdracht 7 --- //
 
 
 function maakBatje(horizontalePositie) {
@@ -22,7 +35,11 @@ function maakBatje(horizontalePositie) {
         breedte: roosterGrootte,
         hoogte: hoogteVanBatje,
         // snelheid van batje
-        dy: 0
+        dy: 0,
+
+        beweegOmhoog: function() { this.dy = -snelheidVanBatje },
+        beweegOmlaag: function() { this.dy = snelheidVanBatje },
+        stop: function() { this.dy = 0 },
     };
 }
 
@@ -31,16 +48,24 @@ function maakMuur(verticalePositie) {
         x: 0, // links
         y: verticalePositie, // verticalePositie
         breedte: speelveld.breedte, // hele breedte van het speelveld
-        hoogte: roosterGrootte // hoogte van het rooster
+        hoogte: roosterGrootte, // hoogte van het rooster
     };
 }
+// --- Opdracht 1 --- //
 
+const muurBovenkant = maakMuur(0); // Helemaal bovenaan
+const muurOnderkant = maakMuur(speelveld.hoogte - roosterGrootte); // Onderkant van het speelveld min de roosterGrootte.
+
+// --- Opdracht 1 --- //
+
+
+// --- Opdracht 2 --- //
 
 const linkerBatje = maakBatje(roosterGrootte * 2); // 2x de roostergrootte vanaf de Linkerkant (2x losse ruimte aan de linkerkant)
-const rechterBatje = maakBatje(speelveld.breedte - roosterGrootte * 3); // 3x de roostergrootte vanaf de Rechterkant (2x losse ruimte + 1x de breedte van het batje)
+const rechterBatje = maakBatje(speelveld.breedte - 3 * roosterGrootte); // 3x de roostergrootte vanaf de Rechterkant (2x losse ruimte + 1x de breedte van het batje)
 
-const muurBovenkant = maakMuur(0);//helemaal bovenaan
-const muurOnderkant = maakMuur(speelveld.hoogte - roosterGrootte);//onderkant van het speelveld min de roosterGrootte.
+// --- Opdracht 2 --- //
+
 
 const balletje = {
     // start in het midden van het spel
@@ -52,9 +77,16 @@ const balletje = {
     // Nodig om bij te houden wanneer het balletje opnieuw in het midden moet beginnen
     resetting: false,
 
+
+    // --- Opdracht 8 --- //
+
     // Snelheid van balletje, begin altijd met naar de rechter boven hoek te bewegen
     dx: snelheidVanBalletje,
-    dy: -snelheidVanBalletje
+    dy: -snelheidVanBalletje,
+
+    // --- Opdracht 8 --- //
+
+    keerOm: function () { this.dy *= -1; },
 };
 
 
@@ -92,10 +124,21 @@ function verwerkBalletje() {
     // Zorg ervoor dat het balletje niet buiten het speelveld kan komen, maar de andere kant op stuitert.
     if (balletje.y < roosterGrootte) {
         balletje.y = roosterGrootte;
-        balletje.dy *= -1;
+
+        // --- Opdracht 4 (a) --- //
+
+        balletje.keerOm();
+
+        // --- Opdracht 4 (a) --- //
+
     } else if (balletje.y + roosterGrootte > speelveld.hoogte - roosterGrootte) {
         balletje.y = speelveld.hoogte - roosterGrootte * 2;
-        balletje.dy *= -1;
+
+        // --- Opdracht 4 (b) --- //
+
+        balletje.keerOm();
+
+        // --- Opdracht 4 (b) --- //
     }
 
     // Zorg ervoor dat de bal opnieuw begint op de start positie zodra deze voorbij een batje komt(Maar alleen als we dat nog niet gedaan hebben).
@@ -108,6 +151,28 @@ function verwerkBalletje() {
             balletje.x = speelveld.breedte / 2;
             balletje.y = speelveld.hoogte / 2;
         }, 500);
+    }
+}
+
+function tekenRooster(kleur, lijnDikte) {
+    context.fillStyle = 'lightblue';
+    context.strokeStyle = kleur;
+    context.lineWidth = lijnDikte;
+
+    // Horizontale lijnen
+    for (let y = 0; y * roosterGrootte <= speelveld.hoogte; y++) {
+        context.beginPath();
+        context.moveTo(0, y * roosterGrootte);
+        context.lineTo(speelveld.breedte, y * roosterGrootte);
+        context.stroke();
+    }
+
+    // Verticale lijnen
+    for (let x = 0; x * roosterGrootte <= speelveld.breedte; x++) {
+        context.beginPath();
+        context.moveTo(x * roosterGrootte, 0);
+        context.lineTo(x * roosterGrootte, speelveld.hoogte);
+        context.stroke();
     }
 }
 
@@ -138,9 +203,18 @@ function loop() {
         balletje.x = rechterBatje.x - balletje.breedte;
     }
 
+    tekenRooster('grey', 1);
+
     tekenVierkant('white', linkerBatje);
     tekenVierkant('white', rechterBatje);
+
+
+    // --- Opdracht 3 --- //
+
     tekenVierkant('white', balletje);
+
+    // --- Opdracht 3 --- //
+
 
     tekenVierkant('lightgrey', muurBovenkant);
     tekenVierkant('lightgrey', muurOnderkant);
@@ -159,33 +233,71 @@ function loop() {
 // Luister naar toets aanslagen om de batjes naar boven of beneden te bewegen
 function luisterToetsIndrukken(e) {
 
-    // Pijltje naar boven zorgt ervoor dat het rechter batje omhoog gaat
     if (e.key === 'ArrowUp' || e.key === 'Up') {
-        rechterBatje.dy = -snelheidVanBatje;
-    }
-    // Pijltje naar onder zorgt ervoor dat het rechter batje omlaag gaat
-    else if (e.key === 'ArrowDown' || e.key === 'Down') {
-        rechterBatje.dy = snelheidVanBatje;
+        // Pijltje naar boven zorgt ervoor dat het rechter batje omhoog gaat
+
+        // --- Opdracht 5 (a) --- //
+
+        rechterBatje.beweegOmhoog();
+
+        // --- Opdracht 5 (a) --- //
+
+    } else if (e.key === 'ArrowDown' || e.key === 'Down') {
+        // Pijltje naar onder zorgt ervoor dat het rechter batje omlaag gaat
+
+        // --- Opdracht 5 (b) --- //
+
+        rechterBatje.beweegOmlaag();
+
+        // --- Opdracht 5 (b) --- //
+
     }
 
-    // De 'w' toets zorgt ervoor dat het linker batje omhoog gaat
-    if (e.key === "w") {
-        linkerBatje.dy = -snelheidVanBatje;
-    }
-    // De 's' toets zorgt ervoor dat het linker batje omlaag gaat
-    else if (e.key === 's') {
-        linkerBatje.dy = snelheidVanBatje;
+    // --- Opdracht 6 (a) --- //
+    if (e.key === 'w') {
+        // De 'w' toets zorgt ervoor dat het linker batje omhoog gaat
+        // --- Opdracht 6 (a) --- //
+
+        // --- Opdracht 5 (c) --- //
+
+        linkerBatje.beweegOmhoog();
+
+        // --- Opdracht 5 (c) --- //
+
+    // --- Opdracht 6 (b) --- //
+    } else if (e.key === 's') {
+        // De 's' toets zorgt ervoor dat het linker batje omlaag gaat
+        // --- Opdracht 6 (b) --- //
+
+        // --- Opdracht 5 (d) --- //
+
+        linkerBatje.beweegOmlaag();
+
+        // --- Opdracht 5 (d) --- //
+
     }
 }
 
 // Luister naar het loslaten van toetsen om te zorgen dat het batje stopt met bewegen
 function luisterToetsLoslaten(e) {
     if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'Up' || e.key === 'Down') {
-        rechterBatje.dy = 0;
+
+        // --- Opdracht 5 (e) --- //
+        rechterBatje.stop();
+        // --- Opdracht 5 (e) --- //
+
     }
 
+    // --- Opdracht 6 (c) --- //
     if (e.key === 'w' || e.key === 's') {
-        linkerBatje.dy = 0;
+    // --- Opdracht 6 (c) --- //
+
+        // --- Opdracht 5 (f) --- //
+
+        linkerBatje.stop();
+
+        // --- Opdracht 5 (f) --- //
+
     }
 }
 
